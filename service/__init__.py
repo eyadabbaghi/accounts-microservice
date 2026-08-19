88386a5ea085f3ab699e8106931a20396b87505f
eyadabbaghi <132089279+eyadabbaghi@users.noreply.github.com>
2026-08-19 10:50:03 +0100

    Update imports and initialize Flask application

--- service/__init__.py
+++ service/__init__.py
@@ -2,12 +2,13 @@ from flask import Flask
 from flask_talisman import Talisman
 from flask_cors import CORS
 
+# Create Flask application
 app = Flask(__name__)
 
-# Initialize Talisman for HTTP security headers
+# Initialize Talisman with security headers
 talisman = Talisman(app)
 
 # Initialize CORS
 CORS(app)
 
-from service import routes
+from service import routes, models, common
